b3a920292bb8419b6ad448a1e22964a5efd2de66
ryanson7 <admin7uk@gmail.com>
2025-10-15 20:42:48 +0900

    fix: Disable webpack cache and exclude from Cloudflare build

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.cache = false;
+    return config;
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  }
-}
-module.exports = nextConfig
+};
+
+module.exports = nextConfig;
